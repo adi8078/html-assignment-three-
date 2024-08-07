bee9a4143ffbf63e37e0433f5902c269dad6a20c
adi8078 <narayankradi@gmail.com>
2024-08-06 23:47:14 -0400

    Add files via upload

---
+++ ecommerce/js/script.js
@@ -0,0 +1,9 @@
+function emailSent(event) {
+    event.preventDefault();
+    alert('Your email has been sent!');
+}
+
+function addedToCart(event) {
+    event.preventDefault()
+    alert('Your item has been added to cart')
+}
